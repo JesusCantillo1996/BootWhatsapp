4190e2e2d9b6205ee11b18d9530d95c4c5040f7b
Jesus Cantillo Sarmiento <jesuscantillosarmiento96@gmail.com>
2023-12-07 13:51:10 -0500

    BootWhatsapp

---
+++ sett.py
@@ -0,0 +1,24 @@
+token = 'bigdateros'
+
+whatsapp_token = 'EAAFNleJR7dsBOwp3MBzEkZBJPZCAjk0yeUZAdOx92qJTVM8LXy7CXJ8HmOArMNhsBTtlWIcfaCaPZCWZAQOuPWivLdd2SG3MdfVuXoHEB4VzC6qNqhCi8HW0N29aJL11hqQyHowQ5ywdj9Vmq17IWFXrLKh6ct9wBolGyxpcDlaD0Rwt3JASnWVv7eHuUBYH9SYPjByycB71h7AZAS0j8ZD'
+
+whatsapp_url ='https://graph.facebook.com/v17.0/203677882819312/messages'
+
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
